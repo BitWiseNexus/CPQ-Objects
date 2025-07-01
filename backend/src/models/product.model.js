3a72e7fe4dc89c9e4e75c70acaffe922d141c1db
BitWiseNexus <adityarehpade1@gmail.com>
2025-07-01 17:50:34 +0530

    product model added

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  sku: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Product', productSchema);
